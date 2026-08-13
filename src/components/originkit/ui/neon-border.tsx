8f10b436d1aa3e5d7a468406e250d5e1f5ca018f
Jaloliddin <jaloliddinxalimov.0102@gmail.com>
2026-08-13 19:42:23 +0500

    uielemntsadded

--- src/components/originkit/ui/neon-border.tsx
+++ src/components/originkit/ui/neon-border.tsx
@@ -368,8 +368,8 @@ export default function NeonBorder(props: Props) {
                 position: "relative",
                 width: "100%",
                 height: "100%",
-                flexShrink: 0,
                 borderRadius: radius,
+                overflow: "clip",
                 ...style,
             }}
         >
